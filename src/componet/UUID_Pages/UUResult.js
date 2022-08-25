fe72e2f2b7f85d93024cb2578d1e5b7619c7d26f
raylyu <raylyu11@gmail.com>
2022-08-24 22:08:57 -0400

    resultpage

--- src/componet/UUID_Pages/UUResult.js
+++ src/componet/UUID_Pages/UUResult.js
@@ -2,24 +2,26 @@ import React from 'react'
 import '../../CSS/App.css'
 export const UUResult = ({result_key,result_value,rlist}) => {
   return (
-    <div className='card' style={{display:"flex", height:"50%"}}>
-      <div style={{display:"flex", flexDirection:"column"}}>
-        {result_key.map((r,idx)=>(
-                  <p key={idx}>{r.toString()}</p>
-              ))}
-      </div>
-            
-      <div style={{display:"flex", flexDirection:"column", flexWrap:"wrap", overflow:"scroll"}}>
-        {result_value.map((r,idx)=>(
-                  <p key={idx}>{r.toString()}</p>
-              ))}
-      </div>
-      <h2>Reteirved Files</h2>
-      <div style={{display:"flex", flexDirection:"row"}}>
-        {rlist.map((r,idx)=>(
-          <p key={idx}>{r.toString()}</p>
-        ))}
-      </div>
+    <div className='card' style={{height:"95vh", overflow:"scroll", display:"flex", flexWrap:"wrap"}}>
+      <table className='dataTable'>
+        <thead>
+          <th>Key</th>
+          <th>Value</th>
+        </thead>
+        <tbody>
+          {result_key.map((rk,idx)=>(
+            <tr>
+              <td key={idx+"key"}>{rk}</td>
+              <td key={idx+"value"} style={{overflow:"scroll", maxWidth:"35vw"}}>
+                {result_value[idx]}</td>
+            </tr>
+          ))}
+          <tr>
+            <td>Retrieved Files</td>
+            <td>{rlist.toString()}</td>
+          </tr>
+        </tbody>
+      </table>
             
 
         
